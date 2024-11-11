d5059d1a75d5b5adf7912ffef30e0846844085cc
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-11-11 08:30:01 +0500

    .

--- index.js
+++ index.js
@@ -7,43 +7,40 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
+connectDB();
 
 // Serve static files from the client/dist directory
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.use(
   cors({
-    origin: ["https://hrlow-server.vercel.app","https://hrflow-client.vercel.app"],
+    origin: ["http://localhost:5173"],
+    // origin:["https://hrflow.vercel.app","https://hrflow-mern-server.vercel.app","https://hrflow-mern-backend-five.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
   );
-  connectDB();
   app.use(cookieParser());
   app.use(express.json());
-  // app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
- 
 const admin = require('./routes/adminRoutes')
 app.use('/api/v1', admin);
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Catch-all route to serve index.html
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/dist/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
 
-
-// Catch-all route to serve index.html
-// app.get("*", function (_, res) {
-//   res.sendFile(
-//     path.join(__dirname, "../client/dist/index.html"),
-//     function (err) {
-//       res.status(500).send(err);
-//     }
-//   );
-// });
-
-
+app.use('/', (req, res) => {
+  res.send('Hello World');
+});
 app.listen(port, () => {
     console.log(`server is running on localhost ${port}`);
 });
